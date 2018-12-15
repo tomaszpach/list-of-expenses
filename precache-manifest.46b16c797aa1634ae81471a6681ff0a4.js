d89e30edb33ae1692bd127b7a6fc956324a188a2
tomaszpach <tomaszpach@icloud.com>
2018-12-15 15:58:52 +0100

    Updates

---
+++ precache-manifest.46b16c797aa1634ae81471a6681ff0a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a1ac6ff471dc08a615",
+    "url": "/list-of-expenses/static/css/main.60c35b4d.chunk.css"
+  },
+  {
+    "revision": "c6a1ac6ff471dc08a615",
+    "url": "/list-of-expenses/static/js/main.c6a1ac6f.chunk.js"
+  },
+  {
+    "revision": "7f4b106228a95ecf782a",
+    "url": "/list-of-expenses/static/js/1.7f4b1062.chunk.js"
+  },
+  {
+    "revision": "edf32cf184958b6352c4",
+    "url": "/list-of-expenses/static/js/runtime~main.edf32cf1.js"
+  },
+  {
+    "revision": "785bee01dd094013c40dcb720e912ff9",
+    "url": "/list-of-expenses/index.html"
+  }
+];
