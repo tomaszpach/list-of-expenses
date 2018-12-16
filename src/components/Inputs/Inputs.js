bbf40ba5782926a730b2999d5a04e355dd165198
tomaszpach <tomaszpach@icloud.com>
2018-12-16 22:59:13 +0100

    RWD part 2, shorted placeholders

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -40,7 +40,7 @@ class Inputs extends Component {
                        pattern=".{5,50}"
                        inputTitle="Minimum 5 characters"
                        type="text" name="title"
-                       placeholder="Please type title (minimum 5 characters)"
+                       placeholder="Minimum 5 characters"
                        onChange={(e) => this.updateInput('title', e)}
                        refe={(input) => {
                            this.nameInput = input
@@ -54,7 +54,7 @@ class Inputs extends Component {
                        inputTitle="Up to 2 numbers after the decimal"
                        type="number"
                        name="amount"
-                       placeholder="Type amount"
+                       placeholder="Example: 73.12"
                        onChange={(e) => this.updateInput('value', e)}/>
 
                 <input className="btn" type="submit" value="add expense"/>
