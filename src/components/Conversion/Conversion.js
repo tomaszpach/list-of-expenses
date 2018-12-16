ecafe504870726ad4f361fbf2392fb8ef2970dfa
tomaszpach <tomaszpach@icloud.com>
2018-12-16 22:03:10 +0100

    updated conversion description

--- src/components/Conversion/Conversion.js
+++ src/components/Conversion/Conversion.js
@@ -4,12 +4,12 @@ import Input from "../Inputs/_input";
 const Conversion = ({conversion, onSubmit, onChange}) => {
     return (
         <form onSubmit={(e) => onSubmit(e)}>
-            <Input title="Change conversion:"
+            <Input title="Type new conversion:"
                    step=".001"
                    required
                    type="number"
                    name="conversion"
-                   placeholder="Change conversion"
+                   placeholder="Example: 4.382"
                    onChange={(e) => onChange(e)}/>
             <input className="btn" type="submit" value="Change conversion"/>
         </form>
